67c143e78afe9ac00544a86c67aa8612933f14f1
выауу <camgal04@gmail.com>
2021-02-28 19:01:01 +0300

    Капучино

--- main.py
+++ main.py
@@ -3,8 +3,71 @@ import sqlite3
 
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWidgets import QTableWidgetItem, QHeaderView
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5.QtWidgets import QTableWidgetItem, QHeaderView, QMessageBox
+
+
+class DialogAddOrChangeCoffee(QDialog):
+    """Диалоговое окно для добавления или изменения кофе"""
+
+    def __init__(self, mode: str, degrees: list, values=()):
+        super().__init__()
+
+        self.res = None
+
+        uic.loadUi("addEditCoffeeForm.ui", self)
+
+        self.setWindowTitle(f'{mode} кофе')
+        self.input_degree.addItems(degrees)
+
+        self.btn.clicked.connect(self.func)
+        if mode == 'Изменение':
+            self.btn.setText('Изменить')
+
+        if values:
+            for value, widget in zip(values,
+                                     [self.input_name, self.input_degree,
+                                      self.rb1, self.input_description,
+                                      self.input_price, self.input_v
+                                      ]):
+                if widget is self.input_degree:
+                    widget.setCurrentText(value)
+                elif widget is self.input_description:
+                    widget.setPlainText(str(value))
+                elif widget is self.rb1:
+                    if value:
+                        self.rb1.toggle()
+                    else:
+                        self.rb2.toggle()
+                else:
+                    widget.setText(str(value))
+
+    def func(self):
+        """Слот, срабатывает по нажатию кнопки,
+        обрабатывает различные случаи ввода"""
+        self.error_lbl.setText('')
+        name, price = self.input_name.text(), self.input_price.text()
+        is_ground_coffee = self.rb1.isChecked()
+        v = self.input_v.text()
+        degree = self.input_degree.currentText()
+        description = self.input_description.toPlainText()
+        try:
+            assert name and price and v and degree and description \
+                   and (self.rb1.isChecked() or self.rb2.isChecked()), \
+                'Не должно быть пустых полей'
+            assert float(price) > 0, 'Цена дожна быть положительной'
+            assert float(v) > 0, 'Объём должен быть положительным'
+            self.res = name, degree, is_ground_coffee, description, price, v
+            self.close()
+        except ValueError:
+            self.error_lbl.setText('Некорректный формат')
+        except AssertionError as ae:
+            self.error_lbl.setText(str(ae))
+
+    def get_values(self) -> tuple or None:
+        """Возращает значения, введённые пользователем"""
+        self.exec_()
+        return self.res
 
 
 class Window(QMainWindow):
@@ -25,16 +88,65 @@ class Window(QMainWindow):
 
         self.load_table()
 
+        self.add_btn.clicked.connect(self.add_coffee)
+        self.change_btn.clicked.connect(self.change_coffee)
+
         mode = QHeaderView.Stretch
         header = self.table.horizontalHeader()
         for i in range(1, self.table.columnCount()):
             header.setSectionResizeMode(i, mode)  # по ширине экрана
 
+    def add_coffee(self):
+        """Добавляет кофе"""
+        res = DialogAddOrChangeCoffee('Добавление',
+                                      [el[0] for el in self.cursor.execute(
+                                          "SELECT title FROM degrees_of_roast"
+                                      ).fetchall()]).get_values()
+        if res is not None:
+            self.cursor.execute(
+                """INSERT INTO coffees 
+                        VALUES(?, ?, (
+                        SELECT id from degrees_of_roast
+                            WHERE title = ?
+                        ), ?, ?, ?, ?)""",
+                (self.cursor.execute("SELECT MAX(id) FROM coffees").fetchone()[
+                     0] + 1,) + res)
+            self.connection.commit()
+            self.load_table()
+
+    def change_coffee(self):
+        """Изменяет кофе"""
+        if self.table.currentItem() is None:
+            QMessageBox.critical(self, "Ошибка",
+                                 "Выберите кофе, которое вы хотите изменить")
+            return
+        now_id = int(self.table.item(self.table.currentItem().row(), 0).text())
+        values = self.cursor.execute(
+            """SELECT coffees.title, degrees_of_roast.title, is_ground_coffee,
+            description, price, volume
+                FROM coffees, degrees_of_roast
+                    ON coffees.degree_of_roast = degrees_of_roast.id
+                        WHERE coffees.id = ?""",
+            (now_id,)).fetchone()
+        res = DialogAddOrChangeCoffee('Изменение', [el[0] for el in self.cursor.execute(
+            "SELECT title FROM degrees_of_roast").fetchall()], values).get_values()
+        if res is not None:
+            self.cursor.execute(
+                """UPDATE coffees
+                        SET title = ?, degree_of_roast = (
+                            SELECT id from degrees_of_roast
+                                WHERE title = ?
+                        ), is_ground_coffee =  ?, description =  ?, 
+                        price =  ?, volume =  ?
+                    WHERE id = ?""", res + (now_id,))
+            self.connection.commit()
+            self.load_table()
+
     def load_table(self):
         """Загружает таблицу из БД"""
         res = self.cursor.execute(
             """SELECT coffees.id, coffees.title, degrees_of_roast.title, is_ground_coffee,
-            coffees.description, coffees.price, coffees.volume
+            description, price, volume
                 FROM coffees, degrees_of_roast
                     ON coffees.degree_of_roast = degrees_of_roast.id""").fetchall()
         self.table.clear()
